9d2edbe2272a5e5e9c28768761e8c0be38bb4cc0
Jeffery Tian <jefftian@uw.edu>
2021-10-13 17:10:10 -0700

    updating

--- tryhw0.py
+++ tryhw0.py
@@ -13,6 +13,9 @@ def neural_net():
     l = [   make_connected_layer(inputs, 32),
             make_activation_layer(RELU),
             make_connected_layer(32, 10),
+            # make_connected_layer(32, 16),
+            # make_activation_layer(LRELU),
+            # make_connected_layer(16, 10),
             make_activation_layer(SOFTMAX)]
     return make_net(l)
 
@@ -31,9 +34,9 @@ batch = 128
 iters = 5000
 rate = .01
 momentum = .9
-decay = .0
+decay = .01
 
-m = softmax_model()
+m = neural_net()
 print("training...")
 train_image_classifier(m, train, batch, iters, rate, momentum, decay)
 print("done")
